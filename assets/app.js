67e19469318c40f3249eba99a63d42731f329e6a
LOUIS JOSEPH Kyllian <lj.kyllian@gmail.com*>
2022-11-21 08:47:31 +0100

    project update

--- assets/app.js
+++ assets/app.js
@@ -6,11 +6,11 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "./styles/app.css";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
 
-import './styles/style.js';
+import "./styles/style.js";
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+console.log("Hello Webpack Encore! Edit me in assets/app.js");
